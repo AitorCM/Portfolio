fd9797de83df3c155990dc4f9d9b05006daa8944
AitorCM <aitor935@gmail.com>
2020-04-24 17:14:23 +0200

    major changes - starter blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,47 +1,83 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
-
 module.exports = {
   siteMetadata: {
-    title: "AitorCM",
-    author: "Aitor Carricondo",
-    description: "A personal website for projects and visibility"
+    title: `Gatsby Starter Blog`,
+    author: {
+      name: `Kyle Mathews`,
+      summary: `who lives and works in San Francisco building useful things.`,
+    },
+    description: `A starter blog demonstrating what Gatsby can do.`,
+    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    social: {
+      twitter: `kylemathews`,
+    },
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+        name: `Gatsby Starter Blog`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/gatsby-icon.png`,
       },
     },
-    'gatsby-plugin-sass',
-    'gatsby-plugin-offline',
+    `gatsby-plugin-react-helmet`,
     {
-      resolve: 'gatsby-source-prismic-graphql',
+      resolve: `gatsby-plugin-typography`,
       options: {
-        repositoryName: 'aitorcm-portfolio', // (required)
-        accessToken: 'MC5YbS1rN1JFQUFDRUFEVUh6.77-9Qe-_ve-_ve-_ve-_vUPvv71v77-977-977-977-977-9HO-_vSzvv70eKO-_vQTvv73vv71w77-977-977-9Xe-_vV_vv70', // (optional)
-        path: '/preview', // (optional, default: /preview)
-        previews: true, // (optional, default: false)
-        pages: [{ // (optional)
-          type: 'Articulo',  // TypeName from prismic
-          match: '/article/:uid', // Pages will be generated under this pattern (optional)
-          path: '/article', // Placeholder page for unpublished documents
-          component: require.resolve('./src/pages/articlePage.js'),
-        }],
-        sharpKeys: [
-          /image|photo|picture/, // (default)
-          'profilepic',
-        ],
-      }
-    } 
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,65 +1,72 @@
-import React from 'react'
-import Helmet from 'react-helmet'
+import React from "react"
+import { Link, graphql } from "gatsby"
 
-import Layout from '../components/layout'
-// import Lightbox from 'react-images'
-import Gallery from '../components/Gallery'
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
 
-import thumb01 from '../assets/images/thumbs/01.jpg'
+const BlogIndex = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+  const posts = data.allMarkdownRemark.edges
 
-import full01 from '../assets/images/fulls/01.jpg'
-
-const DEFAULT_IMAGES = [
-    { id: '1', source: full01, thumbnail: thumb01, caption: 'Photo 1', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'}
-];
-
-class HomeIndex extends React.Component {
-
-    render() {
-        const siteTitle = "AitorCM"
-        const siteDescription = "A personal website for showcasing some projects"
-
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="All posts" />
+      <Bio />
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
         return (
-            <Layout>
-                <Helmet>
-                        <title>{siteTitle}</title>
-                        <meta name="description" content={siteDescription} />
-                </Helmet>
-
-                <div id="main">
-
-                    <section id="one">
-                        <header className="major">
-                            <h2>I'm going to use this website to allocate some of<br />
-                            my electronics and programming projects.</h2>
-                        </header>
-                        <p>In here, I will post different projects of different knowledge areas, such as, electronics projects based on 
-                            Arduino, ESP8266 or Raspberry Pi. Also, every kind of computer and programming projects. Basically, every kind of
-                            project that draws my attention.
-                        </p>
-
-                    </section>
-
-                    <section id="two">
-                        <h2>Recent Work</h2>
-
-                        <Gallery images={DEFAULT_IMAGES.map(({ id, source, thumbnail, caption, description }) => ({
-                            source,
-                            thumbnail,
-                            caption,
-                            description
-                        }))} />
-
-                        <ul className="actions">
-                            <li><a href="articlePage" className="button">Full Portfolio</a></li>
-                        </ul>
-                    </section>
-
-                </div>
-
-            </Layout>
+          <article key={node.fields.slug}>
+            <header>
+              <h3
+                style={{
+                  marginBottom: rhythm(1 / 4),
+                }}
+              >
+                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+            </header>
+            <section>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </section>
+          </article>
         )
-    }
+      })}
+    </Layout>
+  )
 }
 
-export default HomeIndex
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
